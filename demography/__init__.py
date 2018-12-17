876661800c47919c4d7e531c0fe2a5d3ee48d0bd
Carl Lipo <clipo@csulb.edu>
2018-12-17 12:09:05 -0500

    added init files

---
+++ demography/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Copyright (c) 2015.  Mark E. Madsen <mark@madsenlab.org>
+#
+# This work is licensed under the terms of the Apache Software License, Version 2.0.  See the file LICENSE for details.
+
+"""
+Description here
+
+"""
+from demography.network import NetworkModel
